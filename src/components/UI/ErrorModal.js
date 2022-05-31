af596cbf4b04deb18701d4ec1d9c358328c05fa5
zooyaho <jiwoo1268@naver.com>
2022-05-31 23:58:30 +0900

    모달창에 backdrop추가

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -6,18 +6,21 @@ import Button from "./Button";
 
 const ErrorModal = (props) => {
   return (
-    <Card className={styles.modal}>
-      <header className={styles.header}>
-        {/* 모달창도 재사용할 수 있기 때문에 title등 동적으로 관리함. */}
-        <h2>{props.title}</h2>
-      </header>
-      <div className={styles.content}>
-        <p>{props.message}</p>
-      </div>
-      <footer className={styles.actions}>
-        <Button>Okay</Button>
-      </footer>
-    </Card>
+    <div>
+      <div className={styles.backdrop} />
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          {/* 모달창도 재사용할 수 있기 때문에 title등 동적으로 관리함. */}
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button>Okay</Button>
+        </footer>
+      </Card>
+    </div>
   );
 };
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import styles from "./AddUser.module.css";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUdername] = useState("");
@@ -21,45 +22,47 @@ const AddUser = (props) => {
       // 공백으로 입력한 경우
       return;
     }
-    if(enteredAge < 1){
+    if (enteredAge < 1) {
       // 나이를 0이하로 입력한 경우
       return;
     }
 
     //onSubmit 기본 동작을 멈춤
     event.preventDefault();
-    
+
     // App.js에 추가할 userName, userAge 전달
     props.onAddUser(enteredUsername, enteredAge);
     // 초기화
     setEnteredUdername("");
     setEnteredAge("");
-
   };
 
   return (
-    <Card className={styles.input}>
-      {/* htmlFor: props속성이며, js에서 for에 해당함 */}
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={usernameChangeHandler}
-          value={enteredUsername}
-        />
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          id="age"
-          type="number"
-          onChange={ageChangeHandler}
-          value={enteredAge}
-        />
-        <Button type="submit" onClick={addUserHandler}>
-          Add User
-        </Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal title="An error eccured!" message="Something went wrong!"/>
+      <Card className={styles.input}>
+        {/* htmlFor: props속성이며, js에서 for에 해당함 */}
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            onChange={usernameChangeHandler}
+            value={enteredUsername}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            onChange={ageChangeHandler}
+            value={enteredAge}
+          />
+          <Button type="submit" onClick={addUserHandler}>
+            Add User
+          </Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
