5882bae3ebb7d58e00e822747430e6a7adbde6ee
zooyaho <jiwoo1268@naver.com>
2022-05-31 22:17:21 +0900

    username, age의 input에 handler함수 각각 연결

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,13 +1,24 @@
-import React from "react";
-import styles from './AddUser.module.css';
+import React, { useState } from "react";
+import styles from "./AddUser.module.css";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUdername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUdername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   const addUserHandler = (event) => {
     //onSubmit 기본 동작을 멈춤
     event.preventDefault();
+    console.log(enteredAge, enteredUsername);
   };
 
   return (
@@ -15,10 +26,12 @@ const AddUser = (props) => {
       {/* htmlFor: props속성이며, js에서 for에 해당함 */}
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <Button type="submit" onClick={addUserHandler}>Add User</Button>
+        <input id="age" type="number" onChange={ageChangeHandler} />
+        <Button type="submit" onClick={addUserHandler}>
+          Add User
+        </Button>
       </form>
     </Card>
   );
