960dbe2b68de66605c7f9bbc559c79dca8b4f6e0
zooyaho <jiwoo1268@naver.com>
2022-05-31 21:37:31 +0900

    AddUser css 구현

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './Card.module.css';
 
 const Card = (props)=>{
   return(
-    <div>{props.children}</div>
+    <div className={styles.card}>{props.children}</div>
   );
 };
 
