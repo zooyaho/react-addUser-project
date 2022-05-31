764bc51141dd1491fa995aaab5a7aaa384fd4a9d
zooyaho <jiwoo1268@naver.com>
2022-05-31 22:05:08 +0900

    Button 스타일 구현

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from './AddUser.module.css';
 
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -17,7 +18,7 @@ const AddUser = (props) => {
         <input id="username" type="text" />
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
