1ce84b45d96f8f2ef58a754b4e60a56b768c2692
zooyaho <jiwoo1268@naver.com>
2022-05-31 21:27:19 +0900

    Card UI 컴포넌트 생성후 AddUser컴포넌트에 추가

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,23 @@
 import React from "react";
+import Card from "../UI/Card";
 
 const AddUser = (props) => {
-
-  const addUserHandler = (event)=>{
+  const addUserHandler = (event) => {
     //onSubmit 기본 동작을 멈춤
     event.preventDefault();
   };
 
   return (
-    //htmlFor: props속성이며, js에서 for에 해당함
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card>
+      {/* htmlFor: props속성이며, js에서 for에 해당함 */}
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
