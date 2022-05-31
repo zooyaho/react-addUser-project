42b3d934fec4db5e0e1a2d3253fb3403ff0afc3d
zooyaho <jiwoo1268@naver.com>
2022-05-31 22:09:08 +0900

    addUser컴포넌트에 Button컴포넌트 연결

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -18,7 +18,7 @@ const AddUser = (props) => {
         <input id="username" type="text" />
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" />
-        <Button type="submit">Add User</Button>
+        <Button type="submit" onClick={addUserHandler}>Add User</Button>
       </form>
     </Card>
   );
