841e5f79b86aa1b6ff20c373e4b9c7f585a7bfc2
zooyaho <jiwoo1268@naver.com>
2022-06-01 13:53:11 +0900

    useRef를 사용하여 input요소의 값을 가져와 사용하고 변경하는 구현

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./AddUser.module.css";
 
 import Card from "../UI/Card";
@@ -7,21 +7,28 @@ import ErrorModal from "../UI/ErrorModal";
 import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUdername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  // const [enteredUsername, setEnteredUdername] = useState("");
+  // const [enteredAge, setEnteredAge] = useState("");
   // 초기값을 undefined로 설정
   const [error, setError] = useState();
+  // state대신 input에 ref로 연결
+  const nameInputRef = useRef(); 
+  const ageInputRef = useRef(); 
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUdername(event.target.value);
-  };
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const usernameChangeHandler = (event) => {
+  //   setEnteredUdername(event.target.value);
+  // };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   const addUserHandler = (event) => {
     //onSubmit 기본 동작을 멈춤
     event.preventDefault();
+    console.log(nameInputRef.current.value);
+
+    const enteredUsername = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
     
     /* 유효성 검사 */
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
@@ -43,9 +50,16 @@ const AddUser = (props) => {
 
     // App.js에 추가할 userName, userAge 전달
     props.onAddUser(enteredUsername, enteredAge);
-    // 초기화
-    setEnteredUdername("");
-    setEnteredAge("");
+
+    /* 초기화 */
+    // setEnteredUdername("");
+    // setEnteredAge("");
+    // useRef로는 초기화 작업때 dom의 값을 직접 변경하는 방법 밖에 없음. 
+    // dom을 리액트가 아닌 직접 변경하는건 좋은 방법이 아니지만 이런 경우에는 사용해도 괜찮음.
+    // 이렇게 값만 읽을때는 state보다는 ref를 사용하여 코드를 줄이면 좋음.
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
+    
   };
 
   const errorHandler = ()=>{
@@ -63,15 +77,17 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            onChange={usernameChangeHandler}
-            value={enteredUsername}
+            // onChange={usernameChangeHandler}
+            // value={enteredUsername}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            onChange={ageChangeHandler}
-            value={enteredAge}
+            // onChange={ageChangeHandler}
+            // value={enteredAge}
+            ref={ageInputRef}
           />
           <Button type="submit" onConfirm={addUserHandler}>
             Add User
