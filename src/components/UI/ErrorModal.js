3be66fad71c465e7238ac708231404cd01a7f706
zooyaho <jiwoo1268@naver.com>
2022-06-01 13:20:31 +0900

    ReactDom.createPortal()를 사용하여 index.html에 backdrop-root, modal-root div를 만들어 포탈로 해당 div로 컴포넌트가 렌더링되게 함

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,27 +1,49 @@
 import React from "react";
+import ReactDom from "react-dom";
 import styles from "./ErrorModal.module.css";
 
 import Card from "./Card";
 import Button from "./Button";
 
+// 모달을 두개의 컴포넌트로 나눠야 포탈을 사용하기 쉬움.
+/* 본문과 상호작용을 막아줌 */
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        {/* 모달창도 재사용할 수 있기 때문에 title등 동적으로 관리함. */}
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      {/* 본문과 상호작용을 막아줌 */}
-      <div className={styles.backdrop} onClick={props.onConfirm}/>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          {/* 모달창도 재사용할 수 있기 때문에 title등 동적으로 관리함. */}
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
